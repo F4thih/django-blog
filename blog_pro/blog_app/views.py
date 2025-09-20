5c88ace307fba28523b33e47207305b728fe9e32
F4thih <fathihnedumkandathil@gmail.com>
2025-09-20 15:25:07 +0530

    added views code
    added loop in index.html

--- blog_pro/blog_app/views.py
+++ blog_pro/blog_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Blog
 # Create your views here.
 def home(request):
-    blog=Blog.objects.all()
-    return render(request,'index.html', {'blog' : blog})
+    blog_v=Blog.objects.all()
+    return render(request,'index.html', {'blog' : blog_v})
